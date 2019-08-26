d9464608dbfeca50c6e61a20c156ded2da6d7889
Alexander Bonilla <aldaboad@gmail.com>
2019-08-26 02:26:42 -0500

    First Commit

---
+++ facuAdmin/sw.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
+
+workbox.routing.registerRoute(
+  new RegExp('/'),
+  workbox.strategies.networkFirst()
+)
+
+workbox.routing.registerRoute(
+  new RegExp('.*\.js'),
+  workbox.strategies.staleWhileRevalidate()
+)
+
+workbox.routing.registerRoute(
+  new RegExp('(.*)/api/(.*)$'),
+  workbox.strategies.networkFirst()
+)
---
+++ facuAdmin/vue.config.js
@@ -0,0 +1,36 @@
+const WorkboxPlugin = require('workbox-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  productionSourceMap: false,
+
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js'
+      }
+    },
+
+    plugins: [
+      new WorkboxPlugin.InjectManifest({
+        swSrc: path.join(__dirname, 'sw.js')
+      })
+    ]
+  },
+
+  lintOnSave: 'error',
+
+  outputDir: '../facuService/assets',
+
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:1337/api',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
+}
