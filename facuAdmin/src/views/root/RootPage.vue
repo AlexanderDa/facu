1f94142646cfd392d56afa238f0c8d3129969689
Alexander Bonilla <aldaboad@gmail.com>
2019-08-26 08:58:28 -0500

    Vuetify added

---
+++ facuAdmin/src/views/root/RootPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <v-app id="inspire">
+      <v-progress-linear color="primary" indeterminate height="3" />
+      <v-content>
+        <v-container fluid fill-height>
+          <v-layout align-content-center justify-center fill-height wrap>
+            <v-flex xs12 subtitle-1 text-center mb-5>
+              <img width="150" src="@/assets/icon.svg" />
+              <h1>FacuEvent</h1>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-content>
+    </v-app>
+  </div>
+</template>
+
+<script lang="ts">
+import RootPageController from './RootPageController';
+
+export default RootPageController;
+</script>
---
+++ facuAdmin/src/views/root/RootPageController.ts
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({ name: 'RootPage' })
+export default class RootPageController extends Vue {
+
+}
